f2dab9ca1dcef669c6b70486ae315f12faef37c2
otomamayikuy <103938277+yukiyamamoto0804@users.noreply.github.com>
2022-08-22 22:32:26 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import Header from "./components/Header"
+import CalendarData from "./components/CalenderData";
 import './App.css';
+import { initializeApp } from "firebase/app";
+import React, { useState } from 'react';
 
 function App() {
+  const firebaseConfig = {
+    storageBucket: '',
+    projectId: "gakugeki-stage"
+  };
+  const app = initializeApp(firebaseConfig);
+  const [stageSelect, setStageSelect] = useState(false)
+  function setTrue() {
+    setStageSelect(true)
+  }
+  function setFalse() {
+    setStageSelect(false)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {stageSelect && <div className="back" onClick={() => setStageSelect(false)}></div>}
+    <Header></Header>
+    <CalendarData app={app} stageSelect={stageSelect} function1={setTrue} function2={setFalse}/>
+    </>
   );
 }
 
